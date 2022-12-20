c03d5a1450a146bc06a7a72203306661a9dbe20a
Christian Montoya <christian.montoya@consensys.net>
2022-12-20 00:03:42 -0500

    Initial commit

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,4 +1,21 @@
-import { OnRpcRequestHandler } from '@metamask/snap-types';
+import {
+  OnTransactionHandler,
+  OnRpcRequestHandler,
+} from '@metamask/snap-types';
+import { getInsights } from './insights';
+
+/**
+ * Handle an incoming transaction, and return any insights.
+ *
+ * @param args - The request handler args as object.
+ * @param args.transaction - The transaction object.
+ * @returns The transaction insights.
+ */
+export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+  return {
+    insights: await getInsights(transaction),
+  };
+};
 
 /**
  * Get a message from the origin. For demonstration purposes only.
